6d928f2d94f81d79cf509702bdfe5cb47fb8665e
DamiCassinotti <damian.cassinotti@despegar.com>
2020-07-12 12:56:20 -0300

    Intento de refactor de processor

--- processor/src/protocol.rs
+++ processor/src/protocol.rs
@@ -2,7 +2,7 @@ use amiquip::{Connection, Channel, Queue, QueueDeclareOptions, Exchange, Publish
 
 pub struct Protocol<'a> {
     connection: Connection,
-    channel: Channel,
+    // channel: Channel,
     direct_exchange: Exchange<'a>,
     fanout_exchange: Exchange<'a>,
     receiver: Queue<'a>,
@@ -24,12 +24,12 @@ impl Protocol<'static> {
         let mut connection = Connection::insecure_open(host).unwrap();
         let channel = connection.open_channel(None).unwrap();
         let direct_exchange = Exchange::direct(&channel);
-        let fanout_exchange = channel.copy().exchange_declare(ExchangeType::Fanout, topic_places, ExchangeDeclareOptions::default()).unwrap();
+        let fanout_exchange = channel.exchange_declare(ExchangeType::Fanout, topic_places, ExchangeDeclareOptions::default()).unwrap();
         let receiver = channel.queue_declare(receiver_queue, QueueDeclareOptions::default()).unwrap();
 
         Protocol {
             connection,
-            channel,
+            // channel,
             direct_exchange,
             fanout_exchange,
             receiver,
